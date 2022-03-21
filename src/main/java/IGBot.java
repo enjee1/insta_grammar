e96bb98a4fb30b8cee3b31527e340d78d4513d53
Enjee1 <22105805+enjee1@users.noreply.github.com>
2022-03-20 22:59:28 -0400

    Added EventListener class to IGBot class. Tested sending and updating messages to Discord bot and server

--- src/main/java/IGBot.java
+++ src/main/java/IGBot.java
@@ -1,17 +1,51 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
-
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.MessageUpdateEvent;
+import net.dv8tion.jda.api.hooks.AnnotatedEventManager;
+import net.dv8tion.jda.api.hooks.SubscribeEvent;
 import javax.security.auth.login.LoginException;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class IGBot {
-    public static void main(String[] args) throws LoginException, InterruptedException {
-        JDA jda = JDABuilder.createDefault("***INSERT TOKEN***").setStatus(OnlineStatus.ONLINE).build();
-        jda.addEventListener();
-        Thread.sleep(10000);
-        jda.shutdownNow();
+    public static void main(String[] args) throws LoginException, InterruptedException, IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("BotToken"));
+        String botToken = reader.readLine().trim();
+        JDA jda = JDABuilder.createDefault(botToken).setStatus(OnlineStatus.ONLINE).build();
+        jda.awaitReady();
+//        EnumSet<GatewayIntent> gatewayIntents = jda.getGatewayIntents();
+//        IEventManager iEventManager = jda.getEventManager();
+//        List<Guild> guildList = jda.getGuilds();
+//        SelfUser selfUser = jda.getSelfUser();
+        AnnotatedEventManager dedotatedWam = new AnnotatedEventManager();
+        jda.setEventManager(dedotatedWam);
+        dedotatedWam.register(new EventListener());
+        //TODO:Figure out a way to shutdown cleanly without infinite loop.
+        while (true) {
+            Thread.sleep(10000);
+            System.out.println(jda.getGatewayPing());
+        }
+//        jda.shutdownNow();
 
 
     }
+
+    public static class EventListener
+    {
+        @SubscribeEvent
+        public void onMsg(MessageReceivedEvent event)
+        {
+            System.out.printf("%s: %s\n", event.getAuthor().getName(), event.getMessage().getContentDisplay());
+        }
+
+        @SubscribeEvent
+        public void onMsgUpdate(MessageUpdateEvent event)
+        {
+            System.out.printf("%s: %s\n", event.getAuthor().getName(), event.getMessage().getContentDisplay());
+        }
+    }
+
 }
